60eda90f15f126d2707f20ebf1633938c6ec8f6d
Administrator <Administrator@MCGL-4981.COMVIVA.COM>
2019-11-18 01:46:19 +0400

    Updated

--- RetailStoreDiscount/src/com/shopping/Main.java
+++ RetailStoreDiscount/src/com/shopping/Main.java
@@ -24,18 +24,18 @@ class Main {
     	User employee = new User(UserType.EMPLOYEE, "test");
     	
         Item groceryItem = new Product("Bread", 10 , ItemType.GROCERY);
-        Item otherItem = new Product("Mobile", 1000, ItemType.OTHER);
+        Item otherItem = new Product("Mobile", 200, ItemType.OTHER);
         
         DiscountHandler discountHandler = new ThresholdDiscount();
         
         Cart cart = new Cart(employee, discountHandler);
-        cart.add(groceryItem, 5);
-        cart.add(otherItem, 3);
+        cart.add(groceryItem, 2);
+        cart.add(otherItem, 2);
         /*
-         *  Total (20 * 4) + (222 * 4) = 968
-         *  No discount on grocery items = 968 still
-         *  After 30% discount on 4 other items totalling 888 = 701.6
-         *  After 35 dollars off due to price over $700 = 666.59999 or 666.6 
+         * 
+         *  No discount on grocery items
+         *  After 30% discount- 285
+         *   
          */
         System.out.println(cart.total());        
     }
